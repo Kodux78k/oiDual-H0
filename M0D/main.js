11eced6d1d411cc801161addecf2819d40aceaf8
Kodux78k <koduxlive@gmail.com>
2026-02-02 21:52:07 -0300

    Update module imports to use external URLs

--- M0D/main.js
+++ M0D/main.js
@@ -1,3 +1,3 @@
 console.log("Infodose conectado", {"ts":1770079171772,"id":"477fa4b7-c9da-48fe-bf52-76834785def9","meta":{"app":"generated.app"}});
-import "./modules/inline-0.js";
-import "./modules/inline-1.js";
+import "https://kodux78k.github.io/oiDual-H0/M0D/m0ds/di_myFrame.js";
+import "https://kodux78k.github.io/oiDual-H0/M0D/m0ds/di_codeMap.js";
